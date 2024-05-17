73ac123fcb6b5ddea1973c03688c38565316d625
geoalu <amanda.lu@gov.bc.ca>
2024-05-17 15:39:15 -0700

    updating some stuff

--- feature_tokml_shp.py
+++ feature_tokml_shp.py
@@ -2,11 +2,11 @@ import arcpy
 import os 
 
 #add your geodatabase path here
-arcpy.env.workspace=r""
+arcpy.env.workspace=r"Z:\GIS\1_Request\Lands\PowellLakeFloatHomes\PowellLake_FloatHome\PowellLake_FloatHome.gdb"
 arcpy.env.overwriteOutput=True
 
 #add output folder path here
-output_folder = r""
+output_folder = r"\\sfp.idir.bcgov\s164\S63097\Share\GIS\1_Request\Lands\PowellLakeFloatHomes\3_export_data"
 #let's check what's in the database
 fc = arcpy.ListFeatureClasses() 
 #for loop to loop through the database
